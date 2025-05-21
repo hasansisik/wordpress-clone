fef07a925cd908455922a7477795afad34058e09
Hasan Şişik <sisik.hsn@gmail.com>
2025-05-22 01:49:24 +0300

    shadcnui

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('@tailwindcss/postcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false, // Diğer stilleri etkilememek için Tailwind'in temel stillerini devre dışı bırakıyoruz
+  },
+  // Çakışmaları önlemek için tüm Tailwind sınıflarına önek ekliyoruz
+  prefix: 'tw-',
+} 
