06d523cfd126716b1cdf174c5dd84cc36402cc24
Hasan Şişik <sisik.hsn@gmail.com>
2025-05-26 21:19:12 +0300

    fix

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,18 +1,18 @@
-import Layout from "@/components/layout/Layout"
-import Hero1 from "@/components/sections/Hero1"
-import Cta4 from "@/components/sections/Cta4"
-import Services2 from "@/components/sections/Services2"
-import Faqs2 from "@/components/sections/Faqs2"
-import Features1 from "@/components/sections/Features1"
+import Layout from "@/components/layout/Layout";
+import Hero1 from "@/components/sections/Hero1";
+import Cta4 from "@/components/sections/Cta4";
+import Services2 from "@/components/sections/Services2";
+import Faqs2 from "@/components/sections/Faqs2";
+import Features1 from "@/components/sections/Features1";
 
 export default function Home() {
-	return (
-		<>
-				<Hero1 />
-				<Cta4 />
-				<Services2 />
-				<Faqs2 />
-				<Features1 />
-		</>
-	)
+  return (
+    <>
+      <Hero1 />
+      <Cta4 />
+      <Services2 />
+      <Faqs2 />
+      <Features1 />
+    </>
+  );
 }
