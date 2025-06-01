af4351f525735e1ca7174266c6db37b9cf3ecf60
Hasan Şişik <sisik.hsn@gmail.com>
2025-06-01 20:26:10 +0300

    faq

--- components/sections/Faqs1.tsx
+++ components/sections/Faqs1.tsx
@@ -14,7 +14,6 @@ export default function Faqs1({ previewData }: Faqs1Props = {}) {
 	const [data, setData] = useState<any>(null)
 	const dispatch = useDispatch<AppDispatch>()
 	const { faq, loading } = useSelector((state: RootState) => state.faq)
-	console.log("faq1",faq)
 	
 	const [activeItem, setActiveItem] = useState(1);
 	const [key, setKey] = useState(0);
